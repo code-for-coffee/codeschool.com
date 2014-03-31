7013fa1f2e9d8197e3ab61503fd9ac548aa6de3e
James <james@codeforcoffee.org>
2014-03-30 20:43:34 -0500

    section 2.3

---
+++ javascript-3/closures/torpedo.js
@@ -0,0 +1,14 @@
+function assignTorpedo(name, passengerArray) {
+    for (var i = 0; i < passengerArray.length; i++) {
+        if (passengerArray[i] == name) {
+            return function () {
+                alert("Ahoy, " + name + "!\n" +
+                      "Man your post at torpedo #" + (i+1) + "!");
+            };
+        }
+    }
+}
+
+var subPassengers = ["Luke", "Chewie", "Han", "Leia", "Yoda", "Boba"];
+var giveAssignment = assignTorpedo("Chewie", subPassengers);
+giveAssignment();
