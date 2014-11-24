4bc21f0c89fee52a50e6398f058390d60fe4712d
James <jtraverjr@gmail.com>
2014-11-24 15:05:36 -0600

    jasmine testing setup/run/etc
    quick sample

---
+++ jasmine/spec/test.js
@@ -0,0 +1,37 @@
+// test spec
+describe("Sample Jasmine Spec based on", function() {
+
+	it("get item from array", function() {
+
+		var arr = new test();
+		arr.add("hello");
+		arr.add("salut");
+		arr.add("bonjour");
+		var result = arr.get(1);
+
+		expect(result).toEqual("salut");
+
+	});
+
+	it("popped item from array", function() {
+
+		var arr = new test();
+		arr.add("test");
+		arr.add("hello");
+		arr.add("funky");
+		var result = arr.pop();
+
+		expect(result).toEqual("funky");
+
+	});
+
+	it("add item to array", function() {
+
+		var arr = new test();
+		var result = arr.add("hi");
+
+		expect(result).toEqual(true);
+
+	});
+
+});
