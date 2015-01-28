12cb31b4ab86f11775493a3c460f26b224093552
code-for-coffee <jtraverjr@gmail.com>
2015-01-27 22:27:14 -0600

    Javascript Application Design - ch1

---
+++ App Design/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by James on 1/27/15.
+ */
+
+require('config.js');
+require('classes/models.js');
+require('classes/views.js');
+require('classes/collections.js');
+require('classes/routers.js');
+require('classes/controllers.js');
