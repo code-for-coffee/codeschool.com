98eff716875b4c657514111425881f6e8ce766da
James Traver <jtraverjr@gmail.com>
2015-12-15 14:17:27 -0600

    let keyword

--- es6/app/app.js
+++ es6/app/app.js
@@ -1,5 +1,16 @@
 'use strict'
 
+// let allows us to declare a variable for a block of code
+// but only for that block of code
+{
+  let someVariable = 'This variable exist only in this block scope';
+  console.log(someVariable);
+}
+
+if (typeof(someVariable) == 'undefined' || elem == null) {
+  console.log('It isn\'t in scope now');
+}
+
 class Model {
   constructor(properties) {
     this.properties = properties;
